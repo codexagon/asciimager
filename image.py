5170d428c9299b72335f425b0cfcd42f61e206fa
Rithvik <84326832+rithvikr9@users.noreply.github.com>
2025-09-20 20:51:52 +0530

    refactor: replace manual grayscale conversion with PIL's convert()
    Switch from custom grayscale conversion algorithm to using PIL's
    convert() function.

--- image.py
+++ image.py
@@ -4,19 +4,5 @@ def load(img_path):
     return Image.open(img_path)
 
 def grayscale(img):
-    rgb_img = img.convert("RGB")
-    width, height = rgb_img.size
-
-    gray_img = Image.new("L", rgb_img.size, color="white")
-
-    for y in range(height):
-        for x in range(width):
-            pixel = rgb_img.getpixel((x, y))
-            r, g, b = pixel
-
-            gray_pixel = int((0.299 * r) + (0.587 * g) + (0.114 * b))
-
-            gray_img.putpixel((x, y), gray_pixel)
-
-    gray_img.save("output.jpg")
+    gray_img = img.convert("L")
     return gray_img
